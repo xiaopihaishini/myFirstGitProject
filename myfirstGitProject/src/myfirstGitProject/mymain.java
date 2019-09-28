0bee9f4eb12bade104c593ae0be755200905498b
zehua <zehua@qq.com>
2019-09-28 13:03:10 +0800

    wewrwerwer

--- myfirstGitProject/src/myfirstGitProject/mymain.java
+++ myfirstGitProject/src/myfirstGitProject/mymain.java
@@ -4,6 +4,6 @@ public class mymain {
 
 	public static void main(String[] args) {
 		//12121
-		System.out.println(78);
+		System.out.println(7833);
 	}
 }
--- myfirstGitProject/src/myfirstGitProject/whatIsYourName.java
+++ myfirstGitProject/src/myfirstGitProject/whatIsYourName.java
@@ -2,5 +2,5 @@ package myfirstGitProject;
 
 public class whatIsYourName {
 
-	public static final int  i = 11;
+	public static final int  i = 1133;
 }
